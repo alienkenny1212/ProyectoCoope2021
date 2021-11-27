635d29531533ec6872e6d1745a81bad97310a2b2
Juliovela23 <juliovel86@mesoamericana.edu.gt>
2021-11-26 22:21:20 -0600

    Ya funciona el delete, CRUD completo compas

--- coope/urls.py
+++ coope/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.post_list_Socios, name='post_list_Socios'),
     path('detSocio/<int:pk>/', views.post_detail_Socios, name='post_detail_socios'),
     path('newSocio', views.post_new_Socios, name='post_new_socios'),
-      path('detSocios/<int:pk>/edit/', views.post_edit_socios, name='post_edit_socios'),
+    path('detSocios/<int:pk>/edit/', views.post_edit_socios, name='post_edit_socios'),
+    path('deleteSocios/<int:pk>/', views.post_delete_socios, name='post_delete_socios'),
 ]
