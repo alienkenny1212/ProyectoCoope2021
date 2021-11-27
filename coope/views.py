99c6c8f877f9aa8b9397c3aa3b9839049084c7e2
Juliovela23 <juliovel86@mesoamericana.edu.gt>
2021-11-26 21:06:18 -0600

    Se añadio el archivo base.html para uso de plantillas, se añadio detalles de socio al presionar un socio, y boton para agregar uno nuevo

--- coope/views.py
+++ coope/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Socio
+from .forms import PostForm_Socio
+from django.shortcuts import render, get_object_or_404
+
 # Create your views here.
 def post_list_Socios(request):
     socios = Socio.objects.filter(Afiliacion_date__lte=timezone.now()).order_by('Afiliacion_date')
     return render(request, 'Socios/post_list_Socios.html', {'socios': socios})
+
+def post_new_Socios(request):
+    form = PostForm_Socio()
+    return render(request, 'Socios/post_new_Socios.html', {'form': form})
+def post_detail_Socios(request, pk):
+    socios = get_object_or_404(Socio, pk=pk)
+    return render(request, 'Socios/post_detail_Socios.html', {'socios': socios})
