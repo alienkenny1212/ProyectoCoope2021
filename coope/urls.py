fd3f274cbabc4f48a8ce71965d4868e17b880e95
Allan Sac <alienkenny1212@gmail.com>
2021-11-26 22:10:42 -0600

    Adicion de personas

--- coope/urls.py
+++ coope/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list_Socios, name='post_list_Socios'),
+    path('Socios', views.post_list_Socios, name='post_list_Socios'),
     path('detSocio/<int:pk>/', views.post_detail_Socios, name='post_detail_socios'),
     path('newSocio', views.post_new_Socios, name='post_new_socios'),
+    path('detPersona/<int:pk>/', views.post_detail_persona, name='post_detail_persona'),
+    path('newPersona', views.post_new_persona, name='post_new_persona'),
+    path('Personas', views.post_list_personas, name='post_list_personas'),
 ]
--- coope/views.py
+++ coope/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Socio
+from .models import Persona
 from .forms import PostForm_Socio
 from django.shortcuts import render, get_object_or_404
 
@@ -12,6 +13,19 @@ def post_list_Socios(request):
 def post_new_Socios(request):
     form = PostForm_Socio()
     return render(request, 'Socios/post_new_Socios.html', {'form': form})
+
 def post_detail_Socios(request, pk):
     socios = get_object_or_404(Socio, pk=pk)
     return render(request, 'Socios/post_detail_Socios.html', {'socios': socios})
+
+def post_list_personas(request):
+    personas = Persona.objects.order_by('born_date')
+    return render(request, 'Personas/post_list_personas.html', {'personas': personas})
+
+def post_new_persona(request):
+    form = PostForm_Socio()
+    return render(request, 'Personas/post_new_persona.html', {'form': form})
+    
+def post_detail_persona(request, pk):
+    persona = get_object_or_404(Persona, pk=pk)
+    return render(request, 'Personas/post_detail_persona.html', {'persona': persona})
