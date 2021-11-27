149c50419761d0f5fa32ae29c71dea74b12b3db6
Juliovela23 <juliovel86@mesoamericana.edu.gt>
2021-11-26 21:37:54 -0600

    Ya funciona el agregar un nuevo socio, y editar, solo falta delete

--- coope/urls.py
+++ coope/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.post_list_Socios, name='post_list_Socios'),
     path('detSocio/<int:pk>/', views.post_detail_Socios, name='post_detail_socios'),
     path('newSocio', views.post_new_Socios, name='post_new_socios'),
+      path('detSocios/<int:pk>/edit/', views.post_edit_socios, name='post_edit_socios'),
 ]
